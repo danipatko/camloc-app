cee199c47514c086edd0c101238adccdb4649bd0
danipatko <p8.pada@gmail.com>
2023-05-04 11:12:41 +0200

    basic camerax preview activity

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,10 +1,40 @@
 #include <jni.h>
 #include <string>
+#include <opencv2/core/core.hpp>
+#include <opencv2/objdetect/objdetect.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <android/log.h>
+
+#define TAG "native-lib.cpp"
+
+using namespace cv;
+
+std::vector<int> markerIds;
+std::vector<std::vector<cv::Point2f>> markerCorners, rejectedCandidates;
+cv::aruco::DetectorParameters detectorParams = cv::aruco::DetectorParameters();
+cv::aruco::Dictionary dictionary = cv::aruco::getPredefinedDictionary(cv::aruco::DICT_4X4_50);
+cv::aruco::ArucoDetector detector(dictionary, detectorParams);
 
 extern "C" JNIEXPORT jstring JNICALL
-Java_com_dapa_camloc_MainActivity_stringFromJNI(
-        JNIEnv* env,
-        jobject /* this */) {
+Java_com_dapa_jno_MainActivity_stringFromJNI(JNIEnv* env, jobject ) {
     std::string hello = "Hello from C++";
     return env->NewStringUTF(hello.c_str());
+}
+
+extern "C" JNIEXPORT void JNICALL
+Java_com_dapa_jno_MainActivity_balls(JNIEnv* env, jobject inst, jlong mat_address) {
+    Mat &shid = *(Mat *) mat_address;
+    Mat mat;
+    cvtColor(shid, mat, COLOR_BGRA2BGR);
+//    Mat &draw = *(Mat *) draw_address;
+    detector.detectMarkers(mat, markerCorners, markerIds, rejectedCandidates);
+//    draw = mat.clone();
+    if(!markerIds.empty()) {
+        __android_log_print(ANDROID_LOG_INFO, TAG, "Found: %d\n", markerIds[0]);
+//        cv::aruco::drawDetectedMarkers(draw, markerCorners, markerIds);
+    }
+
+    // __android_log_print(ANDROID_LOG_INFO, TAG, "Mat type is: %d\n", mat.type());
+
+    // cv::cvtColor(mat, gray, COLOR_RGB2GRAY);
 }
--- app/src/main/java/com/dapa/camloc/MainActivity.kt
+++ app/src/main/java/com/dapa/camloc/MainActivity.kt
@@ -1,13 +1,47 @@
 package com.dapa.camloc
 
+import android.content.pm.PackageManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TextView
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.dapa.camloc.databinding.ActivityMainBinding
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
+import android.Manifest
+import android.util.Log
+import android.widget.Toast
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageAnalysis
+import androidx.camera.core.ImageProxy
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import org.opencv.android.BaseLoaderCallback
+import org.opencv.android.LoaderCallbackInterface
+import org.opencv.android.OpenCVLoader
+import org.opencv.android.Utils
+import org.opencv.core.Mat
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var cameraExecutor: ExecutorService
+
+    external fun stringFromJNI(): String
+    external fun balls(matAddress: Long)
+
+    private val loader: BaseLoaderCallback = object : BaseLoaderCallback(this) {
+        override fun onManagerConnected(status: Int) {
+            Log.d(TAG, "onManagerConnected $status == 0")
+            when (status) {
+                LoaderCallbackInterface.SUCCESS -> {
+                    System.loadLibrary("opencv_java4")
+                }
+                else -> super.onManagerConnected(status)
+            }
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,19 +50,110 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         // Example of a call to a native method
-        binding.sampleText.text = stringFromJNI()
+        // binding.sampleText.text = stringFromJNI()
+        if (allPermissionsGranted()) {
+            startCamera()
+        } else {
+            ActivityCompat.requestPermissions(this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS)
+        }
+
+        cameraExecutor = Executors.newSingleThreadExecutor()
     }
 
-    /**
-     * A native method that is implemented by the 'camloc' native library,
-     * which is packaged with this application.
-     */
-    external fun stringFromJNI(): String
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == REQUEST_CODE_PERMISSIONS) {
+            if (allPermissionsGranted()) {
+                startCamera()
+            } else {
+                Toast.makeText(this, "Permissions not granted by the user.", Toast.LENGTH_SHORT).show()
+                finish()
+            }
+        }
+    }
+
+    private fun startCamera() {
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
+
+        cameraProviderFuture.addListener({
+            // Used to bind the lifecycle of cameras to the lifecycle owner
+            val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
+            // Preview
+            val preview = Preview.Builder()
+                .build()
+                .also {
+                    it.setSurfaceProvider(binding.viewFinder.surfaceProvider)
+                }
+
+            val imageAnalyzer = ImageAnalysis.Builder()
+                .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
+                .build()
+                .also {
+                    it.setAnalyzer(cameraExecutor, ImageAnalyzer())
+                }
+
+            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+            try {
+                cameraProvider.unbindAll()
+                cameraProvider.bindToLifecycle(this, cameraSelector, preview, imageAnalyzer)
+            } catch(exc: Exception) {
+                Log.e(TAG, "Use case binding failed", exc)
+            }
+
+        }, ContextCompat.getMainExecutor(this))
+    }
+
+    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
+        ContextCompat.checkSelfPermission(baseContext, it) == PackageManager.PERMISSION_GRANTED
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        cameraExecutor.shutdown()
+    }
+
+    inner class ImageAnalyzer() : ImageAnalysis.Analyzer {
+        override fun analyze(image: ImageProxy) {
+            val mat = Mat()
+            Utils.bitmapToMat(image.toBitmap(), mat)
+            balls(mat.nativeObjAddr)
+
+            // Utils.matToBitmap(mat, binding.viewFinder.bitmap)
+            image.close()
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        System.loadLibrary("opencv_java4")
+
+        if (OpenCVLoader.initDebug()) {
+            Log.d(TAG, "OpenCV loaded successfully")
+            loader.onManagerConnected(LoaderCallbackInterface.SUCCESS)
+        } else {
+            Log.d(TAG, "OpenCV not loaded")
+            OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_4_0, this, loader)
+        }
+    }
 
     companion object {
-        // Used to load the 'camloc' library on application startup.
+        // Used to load the 'jno' library on application startup.
         init {
-            System.loadLibrary("camloc")
+            System.loadLibrary("jno")
         }
+
+        private const val TAG = "CameraXApp"
+        private const val REQUEST_CODE_PERMISSIONS = 10
+        private val REQUIRED_PERMISSIONS =
+            mutableListOf (Manifest.permission.CAMERA, Manifest.permission.RECORD_AUDIO).apply {
+                if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.P) {
+                    add(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                }
+            }.toTypedArray()
+
     }
 }
